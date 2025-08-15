2f55d5b19c3ea0394ad0848b14f74a9326f235f4
Levijan20 <shahanatayev29@gmail.com>
2025-08-15 21:18:01 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,13 +1,26 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+Engine = os.getenv("engine")
+Host = os.getenv("host")
+Port = os.getenv("port")
+Name = os.getenv("name")
+User = os.getenv("user")
+Password  = os.getenv("password")
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'ENGINE': 'Engine',
+        'HOST': 'Host',
+        'PORT': 'Port',
+        'NAME': 'Name',
+        'USER': 'User',
+        'PASSWORD': 'Password ',
     }
 }
+
 
 INSTALLED_APPS = ['datacenter']
 
